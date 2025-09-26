2980be92bb857286a66022a09081892922f2388f
bsjc1024 <a01665654@tec.mx>
2025-09-25 21:04:34 -0600

    Interfaz Juego

---
+++ pantallaJuego/pantallaJuego/pantallaJuegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pantallaJuegoApp.swift
+//  pantallaJuego
+//
+//  Created by Bum Soo Jang on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct pantallaJuegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
